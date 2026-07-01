8b8f9464e887a0dd33556c1162a7b3a134f86db8
Yuchen Wang <ywan1238@student.monash.edu>
2026-07-01 21:32:50 +0800

    Submit week 2 studio project

---
+++ week2-studio/ywang-library/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import JSONLab from "./components/JSON.vue"
+</script>
+
+<template>
+  <main>
+    <JSONLab />
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+}
+</style>
