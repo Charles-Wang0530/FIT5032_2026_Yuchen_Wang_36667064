58003fbc9c966d897f75ad94c042e83dd1798c96
Yuchen Wang <ywan1238@student.monash.edu>
2026-07-07 15:20:39 +0800

    Complete week 3 form activities

--- week2-studio/ywang-library/src/App.vue
+++ week2-studio/ywang-library/src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
-import JSONLab from "./components/JSON.vue"
+// import JSONLab from "./components/JSON.vue"
+import Form from "./components/Form.vue"
 </script>
 
 <template>
   <main>
-    <JSONLab />
+    <!-- <JSONLab /> -->
+    <Form />
   </main>
 </template>
 
 <style scoped>
 main {
   width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
